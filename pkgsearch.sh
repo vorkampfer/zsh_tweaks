696b4fa4614f903736e72c1e39b4aac8a5acc7ed
Ian Curtis <71790592+vorkampfer@users.noreply.github.com>
2023-09-27 08:13:02 +0000

    Update pkgsearch.sh

--- pkgsearch.sh
+++ pkgsearch.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+# Simple 1 liner used with grep. I if you have autosuggestion this makes searching for packages really easy
+# Instead of remembering the long file name. I tried doing this as an alias doesn't work for some reason.
+# Usage: ./pkgsearch | grep -i "package name"
 LC_ALL=C pacman -Qi | awk '/^Name/{name=$3} /^Installed Size/{print $4$5, name}' | sort -h
