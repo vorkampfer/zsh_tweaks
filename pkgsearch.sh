ee3260c57642b7036a066204855eb02bb58da010
Ian Curtis <71790592+vorkampfer@users.noreply.github.com>
2023-09-27 08:13:48 +0000

    Update pkgsearch.sh

--- pkgsearch.sh
+++ pkgsearch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Works on Arch based distros only
 # Simple 1 liner used with grep. I if you have autosuggestion this makes searching for packages really easy
 # Instead of remembering the long file name. I tried doing this as an alias doesn't work for some reason.
 # Usage: ./pkgsearch | grep -i "package name"
